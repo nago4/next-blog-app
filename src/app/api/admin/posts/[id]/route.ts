0d5d6c3ac5e33abcacc2ab80ff5811193debf080
rs22095L.nago4 <120536850+nago4@users.noreply.github.com>
2025-01-26 21:55:57 +0900

    あ

--- src/app/api/admin/posts/[id]/route.ts
+++ src/app/api/admin/posts/[id]/route.ts
@@ -1,6 +1,6 @@
 import prisma from "@/lib/prisma";
 import { NextResponse, NextRequest } from "next/server";
-import { Post } from "@prisma/client";
+import { Category } from "@prisma/client";
 
 type RouteParams = {
   params: {
@@ -9,62 +9,22 @@ type RouteParams = {
 };
 
 type RequestBody = {
-  title: string;
-  content: string;
-  coverImageURL: string;
-  categoryIds: string[];
+  name: string;
 };
 
 export const PUT = async (req: NextRequest, routeParams: RouteParams) => {
   try {
     const id = routeParams.params.id;
-    const requestBody: RequestBody = await req.json();
-
-    // 分割代入
-    const { title, content, coverImageURL, categoryIds } = requestBody;
-
-    // categoryIds に該当するカテゴリが存在するか確認
-    const categories = await prisma.category.findMany({
-      where: {
-        id: {
-          in: categoryIds,
-        },
-      },
-    });
-    if (categories.length !== categoryIds.length) {
-      throw new Error("指定されたカテゴリが存在しません");
-    }
-
-    // 中間テーブルのレコードを削除
-    await prisma.postCategory.deleteMany({
-      where: { postId: id },
-    });
-
-    // 投稿記事テーブルにレコードを追加
-    const post: Post = await prisma.post.update({
+    const { name }: RequestBody = await req.json();
+    const category: Category = await prisma.category.update({
       where: { id },
-      data: {
-        title, // title: title の省略形であることに注意。以下も同様
-        content,
-        coverImageURL,
-      },
+      data: { name },
     });
-
-    // 中間テーブルにレコードを追加
-    for (const categoryId of categoryIds) {
-      await prisma.postCategory.create({
-        data: {
-          postId: post.id,
-          categoryId: categoryId,
-        },
-      });
-    }
-
-    return NextResponse.json(post);
+    return NextResponse.json(category);
   } catch (error) {
     console.error(error);
     return NextResponse.json(
-      { error: "投稿記事の変更に失敗しました" },
+      { error: "カテゴリの名前変更に失敗しました" },
       { status: 500 }
     );
   }
@@ -73,14 +33,12 @@ export const PUT = async (req: NextRequest, routeParams: RouteParams) => {
 export const DELETE = async (req: NextRequest, routeParams: RouteParams) => {
   try {
     const id = routeParams.params.id;
-    const post: Post = await prisma.post.delete({
-      where: { id },
-    });
-    return NextResponse.json({ msg: `「${post.title}」を削除しました。` });
+    const category: Category = await prisma.category.delete({ where: { id } });
+    return NextResponse.json({ msg: `「${category.name}」を削除しました。` });
   } catch (error) {
     console.error(error);
     return NextResponse.json(
-      { error: "投稿記事の削除に失敗しました" },
+      { error: "カテゴリの削除に失敗しました" },
       { status: 500 }
     );
   }
