04352b72c2f8ec6f5af94f42b29743b46395fc1f
rs22095L.nago4 <120536850+nago4@users.noreply.github.com>
2025-02-20 20:21:04 +0900

    写真接続機能

--- src/app/_types/CoverImage.ts
+++ src/app/_types/CoverImage.ts
@@ -1,4 +1,24 @@
 export type CoverImage = {
+  filter(
+    arg0: (coverImage: {
+      url:
+        | import("react").Key
+        | import("next/dist/shared/lib/get-img-props").StaticImport
+        | null
+        | undefined;
+    }) => boolean
+  ): unknown;
+  map(
+    arg0: (coverImage: {
+      url:
+        | import("react").Key
+        | import("next/dist/shared/lib/get-img-props").StaticImport
+        | null
+        | undefined;
+      width: string | number | undefined;
+      height: string | number | undefined;
+    }) => import("react").JSX.Element
+  ): import("react").ReactNode;
   url: string;
   width: number;
   height: number;
